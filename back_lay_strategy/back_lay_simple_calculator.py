467ac74ffd2c4a82cdb7c80c861d9581ebfae94a
oplaco <gabriel.alfonsin.espin@gmail.com>
2025-05-04 12:00:34 +0200

    Refactored BackLaySimpleCalculator to solve the equation at the class level and not the instance level.

--- back_lay_strategy/back_lay_simple_calculator.py
+++ back_lay_strategy/back_lay_simple_calculator.py
@@ -5,21 +5,60 @@ from bet import BackLeyGroup
 import sympy as sp
 
 class BackLayBaseCalculator(CalculatorBase):
+    # To be set in each subclass
+    _lay_stake_expr = None 
+    _back_balance_expr = None
+    _lay_balance_expr = None
+     
     def __init__(self,back_ley_group:BackLeyGroup):
         self.bb =back_ley_group.back_bet
         self.lb =back_ley_group.lay_bet
+    
+    def get_subs(self) -> dict:
+        return {
+            self.__class__.bb_stake: self.bb.stake,
+            self.__class__.bb_odds: self.bb.odds,
+            self.__class__.bb_fee: self.bb.fee,
+            self.__class__.lb_odds: self.lb.odds,
+            self.__class__.lb_fee: self.lb.fee
+        }
+    
+    @classmethod
+    def create_symbolic_variables(cls):
+        cls.bb_stake, cls.bb_odds, cls.bb_fee = sp.symbols('bb_stake bb_odds bb_fee')
+        cls.lb_odds, cls.lb_fee = sp.symbols('lb_odds lb_fee')
+        cls.lay_stake = sp.Symbol('lay_stake')
         
-        # Symbolic variables
-        self.create_symbolic_variables()
+    @classmethod
+    def _solve_expression(cls):
+        """Called once per subclass to solve the equation."""
+        obj = cls.__new__(cls)
+        obj.create_symbolic_variables()
+        cls._back_balance_expr = obj.build_back_balance_expr()
+        cls._lay_balance_expr = obj.build_lay_balance_expr()
+        eq = sp.Eq( cls._back_balance_expr, cls._lay_balance_expr)
+        cls._lay_stake_expr = sp.solve(eq, obj.lay_stake)[0]
+    
+    def calculate_stake(self) -> Dict[str, Any]:
 
-        # Define default balance expressions
-        self.back_balance_expr = self.build_back_balance_expr()
-        self.lay_balance_expr = self.build_lay_balance_expr()
+        if self._lay_stake_expr is None:
+            self._solve_expression()
         
-    def create_symbolic_variables(self):
-        self.bb_stake, self.bb_odds, self.bb_fee = sp.symbols('bb_stake bb_odds bb_fee')
-        self.lb_odds, self.lb_fee = sp.symbols('lb_odds lb_fee')
-        self.lay_stake = sp.Symbol('lay_stake')
+        # 5. Substitute numeric values
+        subs = self.get_subs()
+
+        lay_stake_val = self._lay_stake_expr.subs(self.get_subs())
+
+        # 6. Evaluate balances with lay_stake plugged in
+        back_val =  round(self._back_balance_expr.subs(subs | {self.lay_stake: lay_stake_val}),2)
+        lay_val =  round(self._lay_balance_expr.subs(subs | {self.lay_stake: lay_stake_val}),2)
+
+        # 7. Return
+        self.lb.stake = round(lay_stake_val,2)
+        self.risk = round(self.lb.stake*(self.lb.odds-1),2)
+        
+
+        return {"lay_stake": self.lb.stake,"risk":self.risk, "back_balance":back_val,"lay_balance":lay_val}
     
     @abstractmethod
     def build_back_balance_expr(self):
@@ -32,35 +71,6 @@ class BackLayBaseCalculator(CalculatorBase):
         """Build sympy expression representing the balance if the lay bet is won.
         """
         pass
-    
-    def get_subs(self) -> dict:
-        return {
-            self.bb_stake: self.bb.stake,
-            self.bb_odds: self.bb.odds,
-            self.bb_fee: self.bb.fee,
-            self.lb_odds: self.lb.odds,
-            self.lb_fee: self.lb.fee
-        }
-    
-    def calculate_stake(self) -> Dict[str, Any]:
-
-        eq = sp.Eq(self.back_balance_expr, self.lay_balance_expr)
-        sol = sp.solve(eq, self.lay_stake)
-        
-        # 5. Substitute numeric values
-        subs = self.get_subs()
-
-        lay_stake_val = sol[0].subs(subs)
-
-        # 6. Evaluate balances with lay_stake plugged in
-        back_val =  self.back_balance_expr.subs(subs | {self.lay_stake: lay_stake_val})
-        lay_val =  self.lay_balance_expr.subs(subs | {self.lay_stake: lay_stake_val})
-
-        # 7. Return
-        self.lb.stake = round(lay_stake_val,2)
-        self.risk = round(self.lb.stake*(self.lb.odds-1),2)
-
-        return {"lay_stake": self.lb.stake,"risk":self.risk}
 
 class BackLayNormalCalculator(BackLayBaseCalculator):
     def build_back_balance_expr(self):
@@ -90,13 +100,14 @@ class BackLayReimbursementCalculator(BackLayBaseCalculator):
         self.reimbursement = reimbursement
         super().__init__(back_ley_group)
         
-    def create_symbolic_variables(self):
+    @classmethod
+    def create_symbolic_variables(cls):
         super().create_symbolic_variables()
-        self.reimbursement_sy = sp.symbols('reimbursement_sy')
+        cls.reimbursement_sy = sp.Symbol('reimbursement_sy')
         
     def get_subs(self) -> dict:
         base_subs = super().get_subs()
-        base_subs[self.reimbursement_sy] = self.reimbursement
+        base_subs[self.__class__.reimbursement_sy] = self.reimbursement
         return base_subs
 
     def build_back_balance_expr(self):
@@ -126,17 +137,18 @@ class BackLayRolloverCalculator(BackLayBaseCalculator):
         self.expected_rating = expected_rating
         super().__init__(back_ley_group)
         
-    def create_symbolic_variables(self):
+    @classmethod
+    def create_symbolic_variables(cls):
         super().create_symbolic_variables()
-        self.bonus_amount_sy = sp.symbols('bonus_amount')
-        self.remaining_rollover_sy = sp.symbols('remaining_rollover')
-        self.expected_rating_sy = sp.symbols('expected_rating')
+        cls.bonus_amount_sy = sp.symbols('bonus_amount')
+        cls.remaining_rollover_sy = sp.symbols('remaining_rollover')
+        cls.expected_rating_sy = sp.symbols('expected_rating')
         
     def get_subs(self) -> dict:
         subs = super().get_subs()
-        subs[self.remaining_rollover_sy] = self.remaining_rollover
-        subs[self.bonus_amount_sy] = self.bonus_amount
-        subs[self.expected_rating_sy] = self.expected_rating
+        subs[self.__class__.remaining_rollover_sy] = self.remaining_rollover
+        subs[self.__class__.bonus_amount_sy] = self.bonus_amount
+        subs[self.__class__.expected_rating_sy] = self.expected_rating
         return subs
 
     def build_back_balance_expr(self):
